ec4d15276e95eae4eb1a442fe3b93c0b4c6a2223
David Doria <daviddoria@Davids-M1-MacBook-Pro.local>
2024-02-14 14:56:01 -0800

    Fixed another type error

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -24,13 +24,8 @@ const ProductCard = ({ product }: Props) => {
         <h3 className="product-title">{product.title}</h3>
 
         <div className="flex justify-between">
-          <p className="text-black opacity-50 text-lg capitalize">
-            {product.category}
-          </p>
-
           <p className="text-black text-lg font-semibold">
-            <span>{product?.currency}</span>
-            <span>{product?.currentPrice}</span>
+            $<span>{product?.currentPrice}</span>
           </p>
         </div>
       </div>
